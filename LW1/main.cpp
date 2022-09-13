7f48b8389798cfdd3e60c5a9e8bdb5ecbb8a1fd8
Bogdan <bogdan.plockiy@mail.ru>
2022-09-14 00:07:07 +0300

    - readme edited - added comments - edited excel - redesigned functions

--- LW1/main.cpp
+++ LW1/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
-#include <ctime>
 #include <fstream>
+#include <ctime>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 //// заполнение массива случайными числами
@@ -13,111 +14,113 @@ using namespace std;
 //	}
 //}
 
-//����������� �-�� �� �����������
-template<typename T>
-void f1(T *arr)
+// генерация рандомного вещественного числа
+// в интервале [min, max]
+double rand_double(double min, double max)
 {
-	double j = 0.0;
-	for (int i = 0; i < 200; i++, j += 0.2)
-	{
-		arr[i] = j;
+	double f = (double)rand() / RAND_MAX;
+	return min + f * (max - min);
 }
 
-	//k, b - ������������
+// упорядочная ф-ия по возрастанию
+template<typename T>
+void f1(T* arr, int size)
+{
+	//k, b - коэффициент
 	T k = 1;
 	T b = 0;
 
-	for (int i = 0; i < 200; i++)
+	double j = 0.0; // координата y
+	for (int i = 0; i < size; i++, j += 0.2)
 	{
-		arr[i] = k * arr[i] + b;
+		arr[i] = k * j + b;
 	}
 }
 
-//����������� �-�� �� ��������
+// упорядочная ф-ия по убыванию
 template<typename T>
-void f2(T *arr)
+void f2(T* arr, int size)
 {
-	double j = 0.0;
-	for (int i = 0; i < 200; i++, j += 0.2)
-	{
-		arr[i] = j;
-}
-
-	//k, b - ������������
+	//k, b - коэффициенты прямой
 	T k = 1;
 	T b = 0;
 
-	for (int i = 0; i < 200; i++)
+	double j = 0.0; // координата y
+	for (int i = 0; i < size; i++, j += 0.2)
 	{
-		arr[i] = -k * arr[i] + b;
+		arr[i] = -k * j + b;
 	}
 }
 
-//��������� �-��
+// случайная ф-ия
 template<typename T>
-void f3(T *arr)		//to do: ������� ���� ������
+void f3(T* arr, int size)		//to do: чота тут было, но чо-хз
 {
-	//k ���������� �����������  [-25,25]
-
-	for (int i = 0; i < 200; i++)
+	//k изменяется в интервале [-25,25]
+	double j = 0.0; // координата y
+	for (int i = 0; i < size; i++)
 	{
-		T k = T(rand() % 51 + (-25));
-		arr[i] = k; 
-}
+		arr[i] = rand_double(-25, 25);
+	}
 
 }
 
-//�������� ����������� �-��(�����)
+// синусоидальная функция
 template<typename T>
-void f4(T *arr)
+void f4(T* arr, int size)
 {
-	double j = 0.0;
-	for (int i = 0; i < 200; i++, j += 0.2)
+	T a = 1; // масштабирование по x
+	T c = 1; // сдвиг по х
+	T b = 5; // мастабирование по y
+
+	double j = 0.0; // координата y
+	for (int i = 0; i < size; i++, j += 0.2)
 	{
-		arr[i] = j;
+		arr[i] = sin(a * j + c) * b;
 	}
-
-	T a = 1; // �������
-	T c = 1; // ���������
-	T b = 1; // ����� ����
-
-	for (int i = 0; i < 200; i++)
-	{
-		arr[i] = sin(a * arr[i] + c) * b;
-	}
-
 }
 
-//�������� ����������� �-�� (����)
+// пилообразная функция
 template<typename T>
-void f5(T *arr)
+void f5(T* arr, int size)
 {
-	double j = 0.0;
-	for (int i = 0; i < 200; i++, j += 0.2)
+	T a = 1; // масштабирование по x
+	T c = 1; // сдвиг по х
+	T b = 1; // мастабирование по y
+	double j = 0.0; // координата y
+	for (int i = 0; i < size; i++, j += 0.2)
 	{
-		arr[i] = j;
+		arr[i] = asin(sin(j * a + c)) * b;
 	}
-
-	T a = 1; // �������
-	T c = 1; // ���������
-	T b = 1; // ����� ����
-
-	for (int i = 0; i < 200; i++)
-	{
-		arr[i] = asin(sin(arr[i] * a + c)) * b;
-	}
-
 }
 
-//�������� ����������� �-�� (�����������)
+// ступенчатая функция
 template<typename T>
-T f6(T x)
+void f6(T* arr, int size)
 {
-	T a = 1; // �������
-	T c = 1; // ���������
-	T b = 1; // ����� ����
-	T res = asin(sin(x * a + c)) * b;
-	return res;
+	int width = 20;		// длина одной ступени
+	double high = 0.3;	// высота одной ступени
+	double j = 0.0;		// координата y
+	
+	// координата j увеличивается на high (высоту шага),
+	// если по i (координата x) прошло ровно width значений
+	for (int i = 0; i < size; i++, j += ((i % width == 0) ? high : 0.0))
+	{
+		// координата y + шум, зависящий от высоты шага
+		arr[i] = j + rand_double(high / 10, high / 10 * 4);
+	}
+}
+
+// печать массива в поток
+template<typename T>
+void print_arr(T* arr, int size,
+	basic_ofstream<char, char_traits<char>>& stream)
+{
+	for (int i = 0; i < size; i++)
+	{
+		stream << fixed << setprecision(6)
+			<< arr[i] << '\n';
+	}
 }
 
 int main()
@@ -125,34 +128,17 @@ int main()
 	ofstream out("out.txt");
 	srand(time(NULL));
 
-	double* arr = new double[200];
+	// размер массива
+	int size = 200;
 
-	////������������ ������
-	//f1(arr);
+	double* arrd = new double[size];
+	int* arri = new int[size];
 
-	////��������� ������
-	//f2(arr);
+	//вызов генерации случайной функции
+	f1(arrd, size);
 
-	////��������� ����
-	//f3(arr);
-
-	////���������
-	//f4(arr);
-
-	//����
-	f5(arr);
-
-	//// ������ �������
-	//for (int i = 0; i < 200; i++)
-	//{
-	//	cout << arr[i] << endl;
-	//}
-
-	// ������ ������ � ����
-	for (int i = 0; i < 200; i++)
-	{
-		out << arr[i] << '\n';
-	}
+	// вывод массива
+	print_arr(arrd, size, out);
 
 	out.close();
 }
